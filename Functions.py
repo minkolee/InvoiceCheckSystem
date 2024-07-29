af4b31125621db9892a97a1b20008509db71bf36
minkolee <minkolee@gmail.com>
2024-07-29 16:16:30 +0800

    Initial commit

---
+++ Functions.py
@@ -0,0 +1,110 @@
+import os
+from decimal import Decimal
+from datetime import datetime
+import Main
+import openpyxl
+
+
+# 判断输入的发票号码是否有效
+def validate_invoice_number(invoice_number: str):
+    flag1 = invoice_number.isdigit()
+    flag2 = len(invoice_number) == 8 or len(invoice_number) == 20
+
+    # print('{}'.format(len(invoice_number)))
+
+    return flag1 and flag2
+
+
+# 检查发票是否真实
+def find_invoice(invoice_number: str):
+    # print("获取的发票字符串是： {}".format(invoice_number))
+
+    result = []
+
+    # 获取其中的所有XLSX文件
+    file_list = [x for x in os.listdir(Main.DATEBASE_PATH) if x.endswith('xlsx')]
+
+    # print(file_list)
+    # 遍历发票数据库内所有的Excel文件
+    for file_name in file_list:
+        full_path = os.path.join(Main.DATEBASE_PATH, file_name)
+        # print(full_path)
+
+        # 打开每一个Excel文件
+        wb = openpyxl.load_workbook(full_path)
+
+        # 遍历其中的所有表
+        sheet_names = [x for x in wb.sheetnames if x.startswith("信息汇总")]
+        for sheet_name in sheet_names:
+            # 在每一个表的第三列和第四列寻找相同的字符串
+            current_ws = wb[sheet_name]
+            # print(sheet_name)
+            # 只要找到就返回结果
+            for i in range(2, current_ws.max_row + 1):
+                if (invoice_number == current_ws.cell(row=i, column=3).value or
+                        invoice_number == current_ws.cell(row=i, column=4).value):
+                    result.append((invoice_number, current_ws.cell(row=i, column=6).value,
+                                   current_ws.cell(row=i, column=20).value, file_name))
+
+    return result
+
+    # 遍历其中的所有表的第三列和第四列
+
+    # 匹配字符串
+
+    # 如果未找到 返回空列表
+
+    # 如果找到，匹配就退出循环，并且返回找到的信息的列表
+
+
+# 组装最后的结果
+def assemble_find_invoice_result(invoice_list: list, invoice_number: str):
+    result = '发票号码：{} 检查结果：\n'.format(invoice_number)
+    if len(invoice_list) == 0:
+        result += "未找到发票信息，怀疑为假发票"
+        return result
+    else:
+        total_amount = Decimal(0)
+        for each_invoice in invoice_list:
+            result += '发票号码：{}\t 发票抬头：{}\t 价税合计：{}\t 文件名称：{}\n'.format(each_invoice[0], each_invoice[1],
+                                                                                       each_invoice[2], each_invoice[3])
+
+            total_amount += Decimal(each_invoice[2])
+
+        result += "上述合计金额为：{}\n".format(total_amount)
+        return result
+
+
+# 到查重台账中寻找发票号码
+def find_repeated_invoice(invoice_number: str):
+    result = []
+
+    # 根据固定路径打开查重台账
+    wb = openpyxl.load_workbook(Main.REPEAT_DATABASE_PATH)
+    ws = wb.active
+
+    # 遍历每一行第一列匹配发票号码
+    for i in range(1, ws.max_row + 1):
+        if invoice_number == ws.cell(row=i, column=1).value == invoice_number:
+            result.append((invoice_number, ws.cell(row=i, column=2).value, ws.cell(row=i, column=3).value))
+
+    return result
+
+
+# 将查到的结果写入到查重台账中，还要记录首次查验的时间
+def write_found_invoice_to_repeat_database(invoice_list: list):
+    # 打开查重台账
+    wb = openpyxl.load_workbook(Main.REPEAT_DATABASE_PATH)
+
+    ws = wb.active
+
+    start_row = ws.max_row + 1
+
+    for each_invoice in invoice_list:
+        ws.cell(row=start_row, column=1).value = each_invoice[0]
+        ws.cell(row=start_row, column=2).value = each_invoice[1]
+        ws.cell(row=start_row, column=3).value = each_invoice[2]
+        ws.cell(row=start_row, column=4).value = datetime.now()
+        start_row += 1
+
+    wb.save(Main.REPEAT_DATABASE_PATH)
---
+++ Main.py
@@ -0,0 +1,137 @@
+import tkinter as tk
+import os
+import Functions
+
+# 路径配置
+DATEBASE_PATH = os.path.join(os.getcwd(), "发票数据库")
+REPEAT_DATABASE_PATH = os.path.join(os.getcwd(), '查重台账.xlsx')
+
+if __name__ == '__main__':
+
+    # 主窗口
+    MAIN_FRAME = tk.Tk()
+
+    # 标题栏
+    MESSAGE_LABEL = tk.Label(master=MAIN_FRAME, text='请输入发票号码。需要同时查重的发票请勾选查重',
+                             font=("微软雅黑", 14), pady=14)
+
+    # 文字栏
+    CHECK_INVOICE_LABEL = tk.Label(master=MAIN_FRAME, text="请输入发票号码以检查真伪:", font=("微软雅黑", 14),
+                                   border=10)
+    # 发票输入框
+    INVOICE_NUMBER_ENTRY = tk.Entry(master=MAIN_FRAME, font=("微软雅黑", 14), width=25)
+
+    # 是否查重的复选框
+    REPEAT_CHECKBOX = tk.Checkbutton(master=MAIN_FRAME, text="是否同时查重", font=("微软雅黑", 14))
+    REPEAT_CHECKBOX.select()
+
+    # 启动按钮
+    CHECK_INVOICE_BUTTON = tk.Button(master=MAIN_FRAME, text="检查发票", font=("微软雅黑", 14))
+
+    # 结果显示框
+    RESULT_LABEL = tk.Label(master=MAIN_FRAME, font=("微软雅黑", 12), anchor='w', pady=12, padx=12, justify='left')
+
+    # 各种路径
+    # 初始化用户主界面
+    MAIN_FRAME.resizable(width=False, height=False)
+    MAIN_FRAME.geometry('1280x720')
+    MAIN_FRAME.title('发票查验和查重')
+    MESSAGE_LABEL.pack(side=tk.TOP)
+
+    # 初始化显示发票查验提示框
+    CHECK_INVOICE_LABEL.pack(side=tk.TOP, fill=tk.X)
+
+    # 初始化发票查验输入框和对应绑定的值
+    INVOICE_NUMBER = tk.StringVar()
+    INVOICE_NUMBER_ENTRY.pack(side=tk.TOP)
+    INVOICE_NUMBER_ENTRY.config(textvariable=INVOICE_NUMBER)
+
+    # 初始化查重复选框并绑定对应的值
+    IS_CHECK_REPEAT = tk.IntVar()
+    REPEAT_CHECKBOX.config(variable=IS_CHECK_REPEAT)
+    REPEAT_CHECKBOX.pack(side=tk.TOP)
+
+    # 初始化按钮 程序要到最后再绑定处理程序
+    CHECK_INVOICE_BUTTON.pack(side=tk.TOP)
+    RESULT_LABEL.pack(side=tk.TOP, fill=tk.X, )
+
+    # 主程序区域 需要都把元素排布好再做主程序部分
+    # 两个核心变量 INVOICE_NUMBER.get() 用来获取发票号码
+    # IS_CHECK_REPEAT.get() 用来获取是否被选中
+
+    # 程序启动时在结果框体显示初始化信息
+    result_string = ''
+    if not os.path.exists(DATEBASE_PATH):
+        result_string += '无发票数据库，程序无法工作，请检查数据库是否存在\n'
+
+    if not os.path.exists(REPEAT_DATABASE_PATH):
+        result_string += '无查重台账，程序无法工作，请检查文件是否存在\n'
+
+    RESULT_LABEL.config(text=result_string)
+
+    # 按钮绑定的主函数，之后将主函数绑定到按钮上
+    def start_check():
+        CHECK_INVOICE_BUTTON.config(state=tk.DISABLED)
+        MAIN_FRAME.update()
+        RESULT_LABEL.config(text='')
+        # print(IS_CHECK_REPEAT.get())
+        # 设置最终的显示字符串
+        result = ''
+        # 获取发票字符串并检查是否有效
+        invoice_number_string = INVOICE_NUMBER_ENTRY.get().strip()
+        if not Functions.validate_invoice_number(invoice_number_string):
+            result += "发票号码: {} 无效，请输入8位或20位发票号码。".format(invoice_number_string)
+            RESULT_LABEL.config(text=result)
+            CHECK_INVOICE_BUTTON.config(state=tk.NORMAL)
+            INVOICE_NUMBER.set('')
+            return
+
+        # 查重的逻辑
+        # 如果勾选查重 先到查重表里查找相应的号码，如果没有，则进行查真，查真之后，将发票信息写入查重表
+        # 如果勾选查重，找到了查重表里的号码，则提示该发票已经查重过
+
+        # 勾选了查重的情况下
+        if IS_CHECK_REPEAT.get():
+            # 先到查重表里寻找编码看是否能够找到该号码,如果找到直接组装字符串返回
+            repeat_result = Functions.find_repeated_invoice(invoice_number_string)
+            if len(repeat_result) > 0:
+                result += '发票号码：{} 已经存在于查重台账中，属于重复发票。'.format(invoice_number_string)
+                RESULT_LABEL.config(text=result)
+                CHECK_INVOICE_BUTTON.config(state=tk.NORMAL)
+                INVOICE_NUMBER.set('')
+                return
+            # 未找到则要先去验真，然后将验真数据写入查重台账，之后返回结果
+            else:
+                result = '发票号码：{} 不存在于查重台账中，将进行验真。\n'.format(invoice_number_string)
+                # 进行验真, 返回查找的结果
+                found_string = Functions.find_invoice(invoice_number_string)
+                # 验真结果为真, 先写入字符串
+                if len(found_string) > 0:
+                    result += Functions.assemble_find_invoice_result(found_string, invoice_number_string)
+                    # 将结果写入到查重台账中
+                    Functions.write_found_invoice_to_repeat_database(found_string)
+                    result += "已将上述查找结果写入查重台账"
+                    RESULT_LABEL.config(text=result)
+                    CHECK_INVOICE_BUTTON.config(state=tk.NORMAL)
+                    INVOICE_NUMBER.set('')
+                    return
+                else:
+                    # 验真结果为假和未勾选的结果是一样的，直接交给组装函数
+                    RESULT_LABEL.config(
+                        text=Functions.assemble_find_invoice_result(found_string, invoice_number_string))
+                    CHECK_INVOICE_BUTTON.config(state=tk.NORMAL)
+                    INVOICE_NUMBER.set('')
+                    return
+        else:
+            # print("未勾选查重，仅仅验真")
+            RESULT_LABEL.config(
+                text=Functions.assemble_find_invoice_result(Functions.find_invoice(invoice_number_string),
+                                                            invoice_number_string))
+            CHECK_INVOICE_BUTTON.config(state=tk.NORMAL)
+            INVOICE_NUMBER.set('')
+            return
+
+
+    CHECK_INVOICE_BUTTON.config(command=start_check)
+
+    MAIN_FRAME.mainloop()
